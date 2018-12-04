fb91f96f365e616d5c4e9f68281b2eaf274e9cb4
Shriggs <spm1215@gmail.com>
2018-12-03 23:32:08 -0600

    Switch to blocky snake movement
    Using body queue algorithm instead of physics based

--- script.js
+++ script.js
@@ -15,66 +15,84 @@ var config = {
     }
 };
 
-function Body (initX, initY, initVx, initVy, sprite) {
+function Body(initX, initY, initVx, initVy, sprite) {
     this.pos = new Vector2(initX, initY);
     this.vel = new Vector2(initVx, initVy);
     this.sprite = sprite;
-
+    this.init = function(phaser) {
+        this.body = phaser.physics.add.image(this.pos.x, this.pos.y, this.sprite);
+        this.body.setVelocity(this.vel.x, this.vel.y);
+    }
 }
-Body.prototype.init = function (phaser, game) {
-    this.body = phaser.physics.add.image(this.pos.x, this.pos.y, this.sprite);
-    this.body.setVelocity(this.vel.x, this.vel.y);
+
+function Snake(sprite) {
+    this.bodies = [new Body(25, 25, 0, 0, sprite), new Body(75, 25, 0, 0, sprite), new Body(125, 25, 0, 0, sprite), new Body(175, 25, 0, 0, sprite), new Body(225, 25, 0, 0, sprite), new Body(275, 25, 0, 0, sprite), new Body(325, 25, 0, 0, sprite), new Body(375, 25, 0, 0, sprite), new Body(425, 25, 0, 0, sprite)];
+    this.timer = 0;
+    this.direction = null;
+    this.directions = {
+        LEFT: new Vector2(-1, 0),
+        RIGHT: new Vector2(1, 0),
+        UP: new Vector2(0, -1),
+        DOWN: new Vector2(0, 1)
+    };
     this.keys = {
         left: ["keydown_A", "keydown_left"],
         right: ["keydown_D", "keydown_right"],
         up: ["keydown_W", "keydown_up"],
         down: ["keydown_S", "keydown_down"]
     };
-}
-Body.prototype.update = function () {
-    this.body.setVelocity(this.vel.x, this.vel.y);
-}
-Body.prototype.bindKey = function (phaser, name, callback) {
-    this.keys[name].forEach(key => {
-        phaser.input.keyboard.on(key, callback);
-    });
-}
+    this.PERIOD = 10;
+    this.bindKey = function(phaser, name, callback) {
+        this.keys[name].forEach(key => {
+            phaser.input.keyboard.on(key, callback);
+        });
+    }
+    this.init = function(phaser) {
+        this.bodies.forEach(body => {
+            body.init(phaser);
+        });
 
-function Snake (sprite) {
-    // Call Body's constructor
-    Body.call(this, 25, 25, 0, 0, sprite);
-    this.SPEED = 50;
-    this.vLEFT = new Vector2(-this.SPEED, 0);
-    this.vRIGHT = new Vector2(this.SPEED, 0);
-    this.vUP = new Vector2(0, -this.SPEED,);
-    this.vDOWN = new Vector2(0, this.SPEED);
-}
-// Make Snake a child of Body, inheriting its functions
-Snake.prototype = Object.create(Body.prototype);
-Snake.prototype.init = function (phaser, game) {
-    // Call Body's init
-    Body.prototype.init.call(this, phaser, game);
-    let snake = this;
-    Body.prototype.bindKey.call(this, phaser, "left", function () {
-        if (!snake.vel.equals(snake.vRIGHT)) {
-            snake.vel.set(snake.vLEFT.x, snake.vLEFT.y);
+        let snake = this;
+        function canChangeDirection(oppositeDir) {
+            return snake.direction === null || snake.bodies.length <= 1 ||
+                !snake.direction.equals(oppositeDir);
         }
-    })
-    Body.prototype.bindKey.call(this, phaser, "right", function () {
-        if (!snake.vel.equals(snake.vLEFT)) {
-            snake.vel.set(snake.vRIGHT.x, snake.vRIGHT.y);
+        this.bindKey(phaser, "left", function() {
+            if (canChangeDirection(snake.directions.RIGHT)) {
+                snake.direction = snake.directions.LEFT;
+            }
+        })
+        this.bindKey(phaser, "right", function() {
+            if (canChangeDirection(snake.directions.LEFT)) {
+                snake.direction = snake.directions.RIGHT;
+            }
+        })
+        this.bindKey(phaser, "up", function() {
+            if (canChangeDirection(snake.directions.DOWN)) {
+                snake.direction = snake.directions.UP;
+            }
+        })
+        this.bindKey(phaser, "down", function() {
+            if (canChangeDirection(snake.directions.UP)) {
+                snake.direction = snake.directions.DOWN;
+            }
+        })
+    }
+    this.update = function(phaser) {
+        if (this.direction !== null) {
+            if (this.timer >= this.PERIOD) {
+                let head = this.bodies[this.bodies.length - 1];
+                let newBody = new Body(head.pos.x + 50 * this.direction.x,
+                    head.pos.y + 50 * this.direction.y, 0, 0, sprite);
+                newBody.init(phaser);
+                this.bodies.push(newBody);
+                this.bodies.splice(0, 1)[0].body.destroy();
+                this.timer = 0;
+            } else {
+                this.timer++;
+            }
         }
-    })
-    Body.prototype.bindKey.call(this, phaser, "up", function () {
-        if (!snake.vel.equals(snake.vDOWN)) {
-            snake.vel.set(snake.vUP.x, snake.vUP.y);
-        }
-    })
-    Body.prototype.bindKey.call(this, phaser, "down", function () {
-        if (!snake.vel.equals(snake.vUP)) {
-            snake.vel.set(snake.vDOWN.x, snake.vDOWN.y);
-        }
-    })
+    }
 }
 
 var game = new Phaser.Game(config);
@@ -91,9 +109,9 @@ function create() {
     this.add.image(350, 350, 'sky');
 
     snake = new Snake('snake');
-    snake.init(this, game);
+    snake.init(this);
 }
 
 function update() {
-    snake.update();
+    snake.update(this);
 }
